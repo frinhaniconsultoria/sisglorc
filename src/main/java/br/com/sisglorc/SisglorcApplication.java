e37801b40e266f16f6724c0f005b552045b21bcb
frinhanifrinhani <frinhaniconsultoria@gmail.com>
2020-08-25 19:47:50 -0300

    Projeto inicial

---
+++ src/main/java/br/com/sisglorc/SisglorcApplication.java
@@ -0,0 +1,13 @@
+package br.com.sisglorc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisglorcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisglorcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sisglorc/SisglorcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sisglorc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisglorcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
